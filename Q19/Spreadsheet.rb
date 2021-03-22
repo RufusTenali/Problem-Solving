94662f2b65be1929417e673338c14f0acfc752d1
Rufus Tenali <rufustensky@gmail.com>
2021-03-22 15:49:46 +0000

    Create Spreadsheet.rb

---
+++ Q19/Spreadsheet.rb
@@ -0,0 +1,23 @@
+ef colNumber(number)
+ alpha = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+ array = []
+ i = 0
+ while number > 0
+ rem = number % 26 
+  if rem == 0
+    array[i] = "Z"
+    i += 1
+    number = (number / 26) -1
+  else 
+    array[i] = alpha[rem-1]
+    i += 1
+    number = (number/26)
+  end
+end
+array.reverse!
+string = array.join("")
+string.upcase!
+print string
+end
+
+colNumber(5682)

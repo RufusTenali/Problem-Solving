c46409f0eb4ec23c164b535b1f67e2c0cae2bbba
Rufus Tenali <rufustensky@gmail.com>
2021-03-22 15:11:39 +0000

    Create SortByLength.rb

---
+++ SortByLength.rb
@@ -0,0 +1,10 @@
+def sort(str)
+    final = "" # string to hold final answer
+    # this splits string into an array and sorts it by length
+    array = str.split(" ").sort { |a, b| [b.length, b] <=> [a.length, a] }.reverse
+    # convert array back into a string
+    final = array.join(' ')
+    print final
+end
+  
+sort("aab aac aaa I am alone without her")

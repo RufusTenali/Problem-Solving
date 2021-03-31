afb7cdac27e949b14a31703d2e4c33a91d323523
Rufus Tenali <rufustensky@gmail.com>
2021-03-31 15:22:45 +0100

    minitest completed

--- T15/Fibonacci.rb
+++ T15/Fibonacci.rb
@@ -1,3 +1,14 @@
+require "minitest/autorun"
+describe "test" do 
+  it "test" do
+    assert_equal 0, (Fibonacci(0))
+    assert_equal 1, (Fibonacci(1))
+    assert_equal 1, (Fibonacci(2))
+    assert_equal 21, (Fibonacci(8))
+    assert_equal 9227465, (Fibonacci(35))
+  end
+end
+
 # Ruby program for calculating the Nth Fibonacci number. 
 def Fibonacci(number) 
 
@@ -11,4 +22,4 @@ def Fibonacci(number)
     end
 end
 
-print Fibonacci(35) 
+#print Fibonacci(8) 

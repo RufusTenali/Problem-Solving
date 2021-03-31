1ff89499a80bbb4238f77ded17eff2219dbecd3d
Rufus Tenali <rufustensky@gmail.com>
2021-03-31 14:57:03 +0100

    minitest complete

--- T14/Factorial.rb
+++ T14/Factorial.rb
@@ -1,10 +1,19 @@
-class Solution
+require "minitest/autorun"
+describe "test" do 
+  it "test" do
+    assert_equal 1, (Factorial(1))
+    assert_equal 2, (Factorial(2))
+    assert_equal 6, (Factorial(3))
+  end
+end
+
+#class Solution
 def Factorial(num)
     if (0..1).include?(num)
       return 1
     end
     num * Factorial(num - 1)
   end
-end
+#end
   
 #print Factorial(10)

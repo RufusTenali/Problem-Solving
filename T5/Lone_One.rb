5c54aae0f2437377e86ae07de3d6bb8afd07e71c
Rufus Tenali <rufustensky@gmail.com>
2021-03-31 17:58:36 +0100

    Solution fixed

--- T5/Lone_One.rb
+++ T5/Lone_One.rb
@@ -1,24 +1,14 @@
-class Loner
-def loneOne(int)
-    # initialize array, counter and index
-    index = 0
-    array = []
+def loneOne(string)
+    #index = 0
     count = 0
-    #convert int into array
-    array = int.to_s.split(//).map{|chr| chr.to_i}
-    #print array
-    array.each do |num|
-      if array[index] == 1
-        if (array[index-1] != 1 || nil) && (array[index+1] != 1 || nil)
-          count += 1
-        end
-        index += 1
-      else
-        index += 1
+    array = string.to_s.chars
+
+    array.each_with_index do |num, index|
+      if num == "1"
+        count += 1 if index == 0 && array[index + 1] != "1"
+        count += 1 if array[index - 1] != "1" && array[index +1] != "1"
       end
     end
     p count
-end
-end
-  
-#loneOne(11031)
+end  
+loneOne(101)

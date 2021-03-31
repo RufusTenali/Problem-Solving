04581153433e9bb288dcdc603dcdca098621c4d7
Rufus Tenali <rufustensky@gmail.com>
2021-03-31 16:12:21 +0100

    minitest completed

--- T18/LargestInt_Test.rb
+++ T18/LargestInt_Test.rb
@@ -9,6 +9,5 @@ class LI_Test < Minitest::Test
     def test_LI
         assert_equal 500, @large.largest([-10, -10, 5, 2])
         assert_equal 60, @large.largest([1,2,3,4,5])
-
     end
 end
--- T18/LargestIntegers.rb
+++ T18/LargestIntegers.rb
@@ -1,9 +1,20 @@
-class LargestIntegers
+require "minitest/autorun"
+describe "test" do 
+  it "test" do
+    assert_equal 500, (largest([10, 10, 5, 2]))
+    assert_equal 480, (largest([5, 8, 5, 12]))
+    assert_equal 56, (largest([8, 1, -5, 7]))
+    assert_equal 288, (largest([9, 3, 4, 8]))
+  end
+end
+
+#class LargestIntegers
 def largest(array)
     total = 0
     bubble_sort(array)
     array.reverse!
     total = array[0]*array[1]*array[2]
+    #p array
     p total
   end
   
@@ -21,6 +32,6 @@ def largest(array)
       end
     array
 end
-end
+#end
   
-#largest([1,2,3,4,5])
+largest([1,2,3,4,5])

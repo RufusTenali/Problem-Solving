d080162f14ebfe40de897e67215db461e151afd8
Rufus Tenali <rufustensky@gmail.com>
2021-04-01 13:58:46 +0100

    minitest complete

--- T6/DeleteExtra.rb
+++ T6/DeleteExtra.rb
@@ -1,21 +1,29 @@
-class Delete 
-def delete_occurrences(arr, num)
-  check = 0
-  index = 0
-  arr.each do |x|
-    if arr[index] == arr[x]
-      check += 1
-      # index +=1
-    elsif index == arr.size
-      check = 0
-    else
-      # index += 1
-      check += 1
+require "minitest/autorun"
+describe "test" do 
+  it "test" do
+    assert_equal [1, 1], (delete([1, 1, 1, 1], 2)) 
+    assert_equal [13, true, nil], (delete([13, true, 13, nil], 1)) 
+    assert_equal [true, true, true], (delete([true, true, true], 3)) 
+  end
+end
+
+def delete(arr, num)
+  farray = []
+  for i in 0...arr.length
+    if !farray.include?(arr[i])
+      check = arr[i]
+      count = 0
+      arr.each do |element|
+        if element == check
+          if count != num
+            farray.push(check)
+            count += 1
+          end
+        end
+      end
     end
   end
-  p arr
-  #puts check
-  #puts index
+  farray
 end
-end
-#delete_occurrences([1,1,1,1],2)
+
+print delete([1, 1, 1, 1], 2)
--- T6/DeleteExtra_Test.rb
+++ T6/DeleteExtra_Test.rb
@@ -10,5 +10,5 @@ class Delete_Test < Minitest::Test
         assert_equal [1, 1], @del.delete_occurrences([1, 1, 1, 1], 2)
         assert_equal [13, true, nil], @del.delete_occurrences([13, true, 13, nil], 1)
         assert_equal [true, true, true], @del.delete_occurrences([true, true, true], 3) 
-        end
     end
+end
